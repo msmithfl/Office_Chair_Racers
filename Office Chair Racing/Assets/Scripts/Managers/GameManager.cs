75c868f4ddc0e2f47f94252f187bd85e14736499
msmithfl <67169935+msmithfl@users.noreply.github.com>
2022-04-20 19:38:29 -0400

    Basic Game Loop Established
    Error Free Game Loop

--- Office Chair Racing/Assets/Scripts/Managers/GameManager.cs
+++ Office Chair Racing/Assets/Scripts/Managers/GameManager.cs
@@ -7,16 +7,17 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager;
 
+    //script is now only used by other scipts for location purposes
     private void Awake()
     {
-        if (gameManager == null)
-        {
-            gameManager = this;
-            DontDestroyOnLoad(this);
-        }
-        else if (gameManager != null)
-        {
-            Destroy(gameObject);
-        }
+        //if (gameManager == null)
+        //{
+        //    gameManager = this;
+        //    DontDestroyOnLoad(this);
+        //}
+        //else if (gameManager != null)
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 }
