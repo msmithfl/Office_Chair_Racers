30fe91d7a84ae1c2af8c85d04ba26d40a953f002
msmithfl <67169935+msmithfl@users.noreply.github.com>
2022-04-21 01:00:32 -0400

    Final Commit
    Buttons set up

--- Office Chair Racing/Assets/Scripts/Managers/AfterGameMenuManager.cs
+++ Office Chair Racing/Assets/Scripts/Managers/AfterGameMenuManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AfterGameMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject menuCanvas;
+    [SerializeField] private Button firstBtnGameMenu;
 
     public void PlayAgain()
     {
@@ -20,5 +22,6 @@ public class AfterGameMenuManager : MonoBehaviour
     public void SetGameMenuActive()
     {
         menuCanvas.gameObject.SetActive(true);
+        firstBtnGameMenu.Select();
     }
 }
